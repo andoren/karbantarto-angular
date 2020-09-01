fbdab48131bffbe9d689565cb01c48dbfb7a4dc5
misi <mpekar55@gmail.com>
2020-09-01 09:56:41 +0200

    a feladatok letöltése most már a jobservicen keresztül megy ahol dummy adatok vannak egyenlőre

--- web/src/app/components/mainpage/mainpage.component.ts
+++ web/src/app/components/mainpage/mainpage.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ComponentFactoryResolver } from '@angular/core';
 import {JobModel} from '../../Models/JobModel';
 import { AuthguardService } from 'src/app/services/authguard.service';
-import { UserModel } from 'src/app/Models/UserModel';
+import {JobService} from '../../services/job.service';
 
 @Component({
   selector: 'app-mainpage',
@@ -10,61 +10,24 @@ import { UserModel } from 'src/app/Models/UserModel';
 })
 export class MainpageComponent implements OnInit {
 
-  jobs:JobModel[] = [];
-  tempJob:JobModel;
-  constructor(authGuard:AuthguardService) {
+
+  inProgressJobs:JobModel[] = [];
+  newJobs:JobModel[] = []
+  constructor(private authGuard:AuthguardService, private jobService:JobService) {
     this.generateTempData()
    }
 
   ngOnInit(): void {
   }
   generateTempData():void{
-    this.tempJob = new JobModel();
-    this.tempJob.setId(1);
-    this.tempJob.setDescription("Elromlott az emeleti 4-es szobábana wc lehúzójának a kiscicájának az izébizéje.");
-    this.tempJob.setTitle("Elromlott Emelet WC");
-    this.tempJob.setOwner(new UserModel("Kovács Éva"));
-    this.tempJob.setCreatedDate(new Date());
-
-    this.jobs.push(this.tempJob);
-
-    this.tempJob = new JobModel();
-    this.tempJob.setId(2);
-    this.tempJob.setDescription("Elromlott az földszinti 23-as szobábana wc lehúzójának a kiscicájának az izébizéje.");
-    this.tempJob.setTitle("Elromlott Földszint WC");
-    this.tempJob.setOwner(new UserModel("Stuller Istvánné"));
-    this.tempJob.setWorker(new UserModel("Pekár Mihály"));
-    this.tempJob.setCreatedDate(new Date());
-    this.tempJob.setProceedDate(new Date());
-    this.jobs.push(this.tempJob);
-
-    this.tempJob = new JobModel();
-    this.tempJob.setId(3);
-    this.tempJob.setDescription("Elromlott az szenvedély 123-as szobábana wc lehúzójának a kiscicájának az izébizéje.");
-    this.tempJob.setTitle("Elromlott Szenvedély TV");
-    this.tempJob.setOwner(new UserModel("Litauszki János"));
-    this.tempJob.setWorker(new UserModel("Sárkány János"));
-    this.tempJob.setCreatedDate(new Date());
-    this.tempJob.setProceedDate(new Date());
-    this.jobs.push(this.tempJob);
-    this.tempJob = new JobModel();
-    this.tempJob.setId(4);
-    this.tempJob.setDescription("Elromlott az szenvedély 123-as szobábana wc lehúzójának a kiscicájának az izébizéje.");
-    this.tempJob.setTitle("Elromlott Szenvedély TV");
-    this.tempJob.setOwner(new UserModel("Litauszki János"));
-    this.tempJob.setWorker(new UserModel("Sárkány János"));
-    this.tempJob.setCreatedDate(new Date());
-    this.tempJob.setProceedDate(new Date());
-    this.jobs.push(this.tempJob);
-    this.tempJob = new JobModel();
-    this.tempJob.setId(5);
-    this.tempJob.setDescription("Elromlott az szenvedély 123-as szobábana wc lehúzójának a kiscicájának az izébizéje.");
-    this.tempJob.setTitle("Elromlott Szenvedély TV");
-    this.tempJob.setOwner(new UserModel("Litauszki János"));
-    this.tempJob.setWorker(new UserModel("Sárkány János"));
-    this.tempJob.setCreatedDate(new Date());
-    this.tempJob.setProceedDate(new Date());
-    this.jobs.push(this.tempJob);
+    this.jobService.getInProgressJobs().subscribe((jobs)=>{
+   
+      this.inProgressJobs = jobs;
+    });
+    this.jobService.getNewJobs().subscribe((jobs)=>{
+     
+      this.newJobs = jobs;
+    });
   }
   jobIsMine():boolean{
     return false;
@@ -72,4 +35,5 @@ export class MainpageComponent implements OnInit {
   isUserJanitor():boolean{
     return true;
   }
+  
 }
---
+++ web/src/app/services/job.service.ts
@@ -0,0 +1,78 @@
+import { Injectable } from '@angular/core';
+import { JobModel } from '../Models/JobModel';
+import { UserModel } from '../Models/UserModel';
+import { Observable } from 'rxjs';
+import { ObserversModule } from '@angular/cdk/observers';
+import { of } from "rxjs";
+import { delay } from 'rxjs/operators';
+@Injectable({
+  providedIn: 'root'
+})
+export class JobService {
+
+  constructor() { }
+  getNewJobs():Observable<any>{
+ 
+    let jobs:JobModel[] = [];
+    let tempJob:JobModel;
+    tempJob = new JobModel();
+    tempJob.setId(1);
+    tempJob.setDescription("Elromlott az Demens 123-as szobábana wc lehúzójának a kiscicájának az izébizéje.");
+    tempJob.setTitle("Elromlott Demens TV");
+    tempJob.setOwner(new UserModel("Körmendi Szilvia"));
+    tempJob.setCreatedDate(new Date());
+
+    jobs.push(tempJob);
+    tempJob = new JobModel();
+    tempJob.setId(2);
+    tempJob.setDescription("Elromlott az 4-es épület 1230-as szobábana wc lehúzójának a kiscicájának az izébizéje.");
+    tempJob.setTitle("Elromlott Szenvedély WC");
+    tempJob.setOwner(new UserModel("Litauszki János"));
+    tempJob.setCreatedDate(new Date());
+
+    jobs.push(tempJob);  
+    tempJob = new JobModel();
+    tempJob.setId(3);
+    tempJob.setDescription("Elromlott az földsznit 123-as szobábana wc lehúzójának a kiscicájának az izébizéje.");
+    tempJob.setTitle("Elromlott Földszint TV");
+    tempJob.setOwner(new UserModel("Stuller Istvánné"));
+    tempJob.setCreatedDate(new Date());
+
+    jobs.push(tempJob);
+    tempJob = new JobModel();
+    tempJob.setId(4);
+    tempJob.setDescription("Elromlott az 4-es épület 1230-as szobábana wc lehúzójának a kiscicájának az izébizéje.");
+    tempJob.setTitle("Elromlott Szenvedély TV");
+    tempJob.setOwner(new UserModel("Litauszki János"));
+    tempJob.setCreatedDate(new Date());
+    jobs.push(tempJob);  
+  return of(jobs).pipe(delay(1000));
+
+}
+  getInProgressJobs():Observable<any>{
+ 
+      let jobs:JobModel[] = [];
+      let tempJob:JobModel;
+      tempJob = new JobModel();
+      tempJob.setId(4);
+      tempJob.setDescription("Elromlott az Demens 123-as szobábana wc lehúzójának a kiscicájának az izébizéje.");
+      tempJob.setTitle("Elromlott Szenvedély TV");
+      tempJob.setOwner(new UserModel("Körmendi Szilvia"));
+      tempJob.setWorker(new UserModel("Sárkány János"));
+      tempJob.setCreatedDate(new Date());
+  
+      jobs.push(tempJob);
+      tempJob = new JobModel();
+      tempJob.setId(5);
+      tempJob.setDescription("Elromlott az 4-es épület 1230-as szobábana wc lehúzójának a kiscicájának az izébizéje.");
+      tempJob.setTitle("Elromlott Szenvedély TV");
+      tempJob.setOwner(new UserModel("Litauszki János"));
+      tempJob.setWorker(new UserModel("Pekár Mihály"));
+      tempJob.setCreatedDate(new Date());
+     
+      jobs.push(tempJob);  
+    return of(jobs).pipe(delay(1000));
+
+  }
+ 
+}

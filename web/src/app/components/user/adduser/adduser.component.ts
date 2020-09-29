576d6427dd0f2a540a982abc9693d31d9e65ce76
andoren <mpekar55@gmail.com>
2020-09-29 19:31:50 +0200

    useradd form validálva

--- web/src/app/components/user/adduser/adduser.component.ts
+++ web/src/app/components/user/adduser/adduser.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { AreaModel } from 'src/app/Models/AreaModel';
 import { AreaService } from 'src/app/services/area.service';
 
@@ -9,34 +9,28 @@ import { AreaService } from 'src/app/services/area.service';
   styleUrls: ['./adduser.component.css']
 })
 export class AdduserComponent implements OnInit {
-  options: FormGroup;
+
   areas:AreaModel[]=[];
   choosenAreas:AreaModel[] = [];
   choosenArea:AreaModel;
-
+  userForm:FormGroup;
   newUsername:String = "";
   newPassword:String = "";
   newFullname:String=""
-  usernameFormControl = new FormControl(this.newUsername,[
-    Validators.required,
-    Validators.minLength(5),
-    Validators.maxLength(100)
-  ]);
-  fullnameFormControl = new FormControl(this.newFullname,[
-    Validators.required,
-    Validators.minLength(10),
-    Validators.maxLength(100)
-  ]);
-  passwordFormControl = new FormControl(this.newPassword,[
-    Validators.required,
-    Validators.minLength(8),
-    Validators.maxLength(50)
-  ]); 
-  roleFormControl = new FormControl();
-  areaFormControl = new FormControl();
-  constructor(private areaService:AreaService) { }
+
+  constructor(private areaService:AreaService,private formBuilder: FormBuilder) {
+    this.userForm = this.formBuilder.group({
+      fullname: [null, [Validators.required, Validators.minLength(7),Validators.maxLength(50)]],
+      username: [null, [Validators.required, Validators.minLength(4),Validators.maxLength(15)]],
+      password: [null, [Validators.required, Validators.minLength(6),Validators.maxLength(12)]],
+      role: [null, [Validators.required]]
+    });
+    
+   }
 
   ngOnInit(): void {
+ 
+  
     this.areaService.getAllArea().subscribe((areas)=>{
       this.areas = areas;
     },error=>{
@@ -60,5 +54,8 @@ export class AdduserComponent implements OnInit {
 
     this.areas.push(area)
 
+  }
+  submit():void{
+    console.log(this.userForm);
   }
 }
